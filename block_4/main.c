7377196e1aad263fbf175e5b11938b545fb18d66
unknown <_Ganimed@ganimed-xp.(none)>
2012-06-18 17:26:09 +0200

    added exercise 7 & 2, changed some parts in 4 and 5 ...

--- block_4/main.c
+++ block_4/main.c
@@ -20,13 +20,13 @@ void main() {
   int node = 0;
   node = 0;
   for(i = 0; i < K; i++) {
-      next = nondet();
+      next = nondet() % N;
       result = result && G[node][next];
       node = next;
   }
    
   printf("result=%d; node=%d", result, node);
-  result = result && (node == 0);
+  result = result && (node == 0) && (K > 0);
    
  assert(!result);
   
